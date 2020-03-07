07d3bd0eb00eb674860c79a39e6acb6d687b1b0c
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-03-07 13:58:47 -0300

    criacao do projeto

---
+++ matatona_ambev/comprador/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CompradorConfig(AppConfig):
+    name = 'comprador'
---
+++ matatona_ambev/distribuidor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DistribuidorConfig(AppConfig):
+    name = 'distribuidor'
