2569de24f7e36a6d74cb6704bd4c5066a90de32e
Pierre Carvalho <pierrefenner@hotmail.com>
2020-03-12 14:48:32 -0300

    Foi criado uma visão distribuidor

--- matatona_ambev/distribuidor/urls.py
+++ matatona_ambev/distribuidor/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from .views import distribuidorCreateView,ListagemView
+from .views import distribuidorCreateView,ListagemView,HomeView
 
 app_name = 'distribuidor'
 urlpatterns = [
+    path('home/', HomeView.as_view(), name='distribuidor_home'),
     path('cadastrar/', distribuidorCreateView.as_view(), name='distribuidor_create'),
     path('listar/', ListagemView.as_view(), name='distribuidor_list'),
 ]
--- matatona_ambev/distribuidor/views.py
+++ matatona_ambev/distribuidor/views.py
@@ -9,14 +9,18 @@ from django.conf import settings
 
 from .models import Distribuidor
 
+class HomeView(TemplateView):
+    template_name = 'distribuidor_home.html'
+
 class distribuidorCreateView(CreateView):
     model = Distribuidor
     template_name = "distribuidor_form.html"
     fields = ['nome_distribuidor', 'email', 'tel_fixo','tel_celular','cep','cidade','lat','lon']
-    success_url = 'distribuidor:distribuidor_list'
-    
+    success_url = 'landpage:home'
+
     def get_success_url(self):
         return reverse(self.success_url)
+
 class PedidoView(TemplateView):
     template_name = "pedido.html"
 
