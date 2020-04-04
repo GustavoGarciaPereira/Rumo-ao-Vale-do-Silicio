777919da90c738550c2eb301a609a5baf0c5544d
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-04-04 15:29:47 -0300

    Deploy da aplicação

--- matatona_ambev/maratona_ambev/settings.py
+++ matatona_ambev/maratona_ambev/settings.py
@@ -78,11 +78,25 @@ WSGI_APPLICATION = 'maratona_ambev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',            'nameofapp.herokuapp.com']
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+
+
     }
+
 }
 
 
