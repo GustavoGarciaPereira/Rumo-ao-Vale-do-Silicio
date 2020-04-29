6d56f24948e965b8d43487733aa701482c2bed18
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-04-29 00:03:28 -0300

    filtragem

--- maratona_ambev/urls.py
+++ maratona_ambev/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('distribuidor/', include('distribuidor.urls')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
